08f77e62c5469f7880afd5a05ae42cb70e11f017
Changlin Li <mail@changlinli.com>
2023-12-05 12:41:42 -0800

    First commit

---
+++ src/ZokkaBasicCustomDependency.elm
@@ -0,0 +1,16 @@
+module ZokkaBasicCustomDependency exposing
+    ( aCustomValue
+    )
+
+{-|
+
+# A new custom value to use in your package
+
+@docs aCustomValue
+
+-}
+
+{-| A fun little custom number. (It's just 1000)
+-}
+aCustomValue : Int
+aCustomValue = 1000
